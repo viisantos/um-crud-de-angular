a6240a8241542abca1e099fd7496bcc4eed4c742
Vitoria <vitoria10_@live.com>
2020-09-10 21:25:42 -0300

    1a parte do tutorial

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppMaterialModule } from '../app-material/app-material.module';
-import { ProductFormComponent } from './product-form/product-form.component';
+//import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products/products.component';
 
 @NgModule({
-  declarations: [ProductsComponent, ProductFormComponent],
+  declarations: [ProductsComponent ], /*, ProductFormComponent*/
   imports: [
     CommonModule,
     ReactiveFormsModule,
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,23 +1,24 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-
 import { Product } from './product';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError, from, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private products: Product[] = [];
-
-  constructor(private http: HttpClient) {
-    for (let i = 1; i <= 10; i++) {
-      this.products.push({id: i, name: `Product ${i}`});
-    }
+  private products: Product[];
+  constructor(private http: HttpClient ) {
+      this.products =[
+         {id: 1, name: 'produto1'},
+         {id: 2, name: 'produto2'},
+         {id: 3, name: 'produto3'},
+         {id: 4, name: 'produto4'},
+         {id: 5, name: 'produto5'}
+      ];
    }
-
-  listAll(): Observable<Product[]> {
-    return of(this.products);
-  }
+   listAll(): Observable<Product[]> {
+     return of(this.products);
+   }
 }
