dc79eda0ee6886399695412690a064c6aafb4ed5
Vitoria <vitoria10_@live.com>
2020-09-26 22:05:59 -0300

    Finalização dos passos da live #4

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-//import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [
-  { path: '', component: ProductsComponent }
-  //{ path: 'create', component: ProductFormComponent }
+const routes: Routes = [ //Rotas para os respectivos componentes
+  { path: '', component: ProductsComponent },
+  { path: 'novo', component: ProductFormComponent }
 ];
 
 @NgModule({
